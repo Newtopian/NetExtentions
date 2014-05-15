9aee6f2e0a5f47fddc25cbe9e7cebfac42352435
Éric Daigneault <santaroga@gmail.com>
2014-05-15 17:12:26 -0400

    First submit of NetExtentions

---
+++ src/StringExtentions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NetExtentions
+{
+    public static class StringExtentions
+    {
+        public static Boolean ContainsIgnoreCase(this String aSource, String aValue)
+        {
+            return aSource != null && aSource.IndexOf(aValue, StringComparison.InvariantCultureIgnoreCase) >= 0;
+        }
+
+        public static Boolean EqualsIgnoreCase(this String aSource, String aValue)
+        {
+            return (String.IsNullOrWhiteSpace(aSource) && String.IsNullOrWhiteSpace(aValue)) || aSource != null && aSource.Equals(aValue, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static Boolean StartsWithIgnoreCase(this String aSource, String aValue)
+        {
+            return aSource != null && aSource.StartsWith(aValue, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static Boolean EndsWithIgnoreCase(this String aSource, String aValue)
+        {
+            return aSource != null && aSource.EndsWith(aValue, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static int IndexOfIgnoreCase(this String aSource, String aValue)
+        {
+            return aSource.IndexOf(aValue, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
